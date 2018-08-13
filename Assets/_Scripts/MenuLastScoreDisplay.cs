20d34f592da556f8211b853ffc8887c1f0327a12
Isaac Doré <swessmusic@gmail.com>
2018-08-12 21:37:36 -0400

    Fix Last Score reset bug

--- Assets/_Scripts/MenuLastScoreDisplay.cs
+++ Assets/_Scripts/MenuLastScoreDisplay.cs
@@ -8,6 +8,9 @@ public class MenuLastScoreDisplay : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		// Rest score
+		GameController.Instance.score = 0;
+
 		Text text = GetComponent<Text>();
 		text.text = "Last Score : " + GameController.Instance.saveFile.lastScore;
 	}
